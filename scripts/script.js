ee623503262f66070a55407b67ffb09bc67faec0
Mahdi A <mahdi.alizadeh.sp@gmail.com>
2022-08-31 23:20:09 +0430

    create play ground table

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+// <get elemets
+var playGround = document.querySelector(".playGround");
+// get elemets>
+// <create board
+// <create columns
+var x;
+for (x = 0; x < 20; x++) {
+    // <create row
+    var row = document.createElement('div');
+    row.className = "row";
+    row.id = "y".concat(x + 1);
+    playGround === null || playGround === void 0 ? void 0 : playGround.appendChild(row);
+    var i = void 0;
+    for (i = 0; i < 30; i++) {
+        var cell = document.createElement("div"); // create one cell
+        // <initial styles for cell
+        cell.className = "cell";
+        cell.id = "x".concat(i + 1);
+        // initial styles for cell>
+        row === null || row === void 0 ? void 0 : row.appendChild(cell);
+    }
+    // create row>
+}
+// create columns>
+// create board>
---
+++ scripts/script.ts
@@ -0,0 +1,26 @@
+// <get elemets
+const playGround: Element | null = document.querySelector(".playGround");
+// get elemets>
+
+// <create board
+        // <create columns
+            let x: number;
+            for(x = 0; x < 20 ; x++) {
+                // <create row
+                    const row: HTMLDivElement = document.createElement('div');
+                    row.className = "row";
+                    row.id = `y${x+1}`
+                    playGround?.appendChild(row);
+                    let i: number;
+                    for(i = 0 ; i < 30 ; i++) {
+                        const cell: HTMLDivElement = document.createElement("div"); // create one cell
+                            // <initial styles for cell
+                            cell.className = "cell"
+                            cell.id = `x${i+1}`
+                            // initial styles for cell>
+                            row?.appendChild(cell);
+                    }
+                // create row>
+            }
+        // create columns>
+// create board>
