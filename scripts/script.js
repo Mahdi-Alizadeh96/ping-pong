16f2331c1862e6e444f8a631f868a28fa2e45bab
Mahdi A <mahdi.alizadeh.sp@gmail.com>
2022-09-01 14:43:45 +0430

    set ball motion

--- scripts/script.js
+++ scripts/script.js
@@ -23,3 +23,66 @@ for (x = 0; x < 20; x++) {
 }
 // create columns>
 // create board>
+// <ball motion
+setInterval(function () {
+    // ballAction();
+}, 50);
+var ballYPosition = 10;
+var ballXPosition = 16;
+var motionX = false; // for left to right motion its false and for right to lef its true
+var motionY = false; // for up to down its false and for down to up its true
+function ballAction() {
+    var getAllCells = document.querySelectorAll(".cell"); // get all cells
+    getAllCells.forEach(function (item) { return item.classList.remove("ball"); }); // remove ball class from all cells
+    var ballPositon = document.querySelector("#y".concat(ballYPosition, " > #x").concat(ballXPosition)); // position of ball;
+    if (!motionX && !motionY) {
+        if (ballYPosition === 20) {
+            motionY = true;
+        }
+        else if (ballXPosition === 31) {
+            motionX = true;
+        }
+        else {
+            ballYPosition++;
+            ballXPosition++;
+        }
+    }
+    if (!motionX && motionY) {
+        if (ballXPosition === 31) {
+            motionX = true;
+        }
+        else if (ballYPosition === 1) {
+            motionY = false;
+        }
+        else {
+            ballYPosition--;
+            ballXPosition++;
+        }
+    }
+    if (motionX && motionY) {
+        if (ballYPosition === 1) {
+            motionY = false;
+        }
+        else if (ballXPosition === 1) {
+            motionX = false;
+        }
+        else {
+            ballYPosition--;
+            ballXPosition--;
+        }
+    }
+    if (motionX && !motionY) {
+        if (ballXPosition === 1) {
+            motionX = false;
+        }
+        else if (ballYPosition === 20) {
+            motionY = true;
+        }
+        else {
+            ballYPosition++;
+            ballXPosition--;
+        }
+    }
+    ballPositon === null || ballPositon === void 0 ? void 0 : ballPositon.classList.add("ball"); // create ball
+}
+// ball motion>
--- scripts/script.ts
+++ scripts/script.ts
@@ -3,24 +3,88 @@ const playGround: Element | null = document.querySelector(".playGround");
 // get elemets>
 
 // <create board
-        // <create columns
-            let x: number;
-            for(x = 0; x < 20 ; x++) {
-                // <create row
-                    const row: HTMLDivElement = document.createElement('div');
-                    row.className = "row";
-                    row.id = `y${x+1}`
-                    playGround?.appendChild(row);
-                    let i: number;
-                    for(i = 0 ; i < 31 ; i++) {
-                        const cell: HTMLDivElement = document.createElement("div"); // create one cell
-                        // <initial styles for cell
-                        cell.className = "cell"
-                        cell.id = `x${i+1}`
-                        // initial styles for cell>
-                        row?.appendChild(cell);
-                    }
-                // create row>
-            }
-        // create columns>
+    // <create columns
+        let x: number;
+        for(x = 0; x < 20 ; x++) {
+            // <create row
+                const row: HTMLDivElement = document.createElement('div');
+                row.className = "row";
+                row.id = `y${x+1}`
+                playGround?.appendChild(row);
+                let i: number;
+                for(i = 0 ; i < 31 ; i++) {
+                    const cell: HTMLDivElement = document.createElement("div"); // create one cell
+                    // <initial styles for cell
+                    cell.className = "cell"
+                    cell.id = `x${i+1}`
+                    // initial styles for cell>
+                    row?.appendChild(cell);
+                }
+            // create row>
+        }
+    // create columns>
 // create board>
+
+// <ball motion
+
+    setInterval(() => {
+        // ballAction();
+    },50);
+
+    let ballYPosition: number = 10;
+    let ballXPosition: number = 16;
+    let motionX: boolean = false // for left to right motion its false and for right to lef its true
+    let motionY: boolean = false // for up to down its false and for down to up its true
+
+    function ballAction() {
+
+        const getAllCells = document.querySelectorAll(".cell"); // get all cells
+        getAllCells.forEach(item => item.classList.remove("ball")); // remove ball class from all cells
+        const ballPositon: Element | null = document.querySelector(`#y${ballYPosition} > #x${ballXPosition}`); // position of ball;
+
+        if(!motionX && !motionY) {
+            if (ballYPosition === 20) {
+                motionY = true;
+            } else if(ballXPosition === 31) {
+                motionX = true;
+            } else {
+                ballYPosition++;
+                ballXPosition++;
+            }
+        }
+        if(!motionX && motionY) {
+            if(ballXPosition === 31) {
+                motionX = true;
+            } else if (ballYPosition === 1) {
+                motionY = false;
+            } else {
+                ballYPosition--;
+                ballXPosition++;
+            }
+        }
+        if(motionX && motionY) {
+            if(ballYPosition === 1) {
+                motionY = false
+            } else if (ballXPosition === 1) {
+                motionX = false;
+            } else {
+                ballYPosition--;
+                ballXPosition--;
+            }
+        }
+        if(motionX && !motionY) {
+            if (ballXPosition === 1) {
+                motionX = false;
+            } else if (ballYPosition === 20) {
+                motionY = true;
+            } else {
+                ballYPosition++;
+                ballXPosition--;
+            }
+        }
+
+
+        ballPositon?.classList.add("ball"); // create ball
+    }
+
+// ball motion>
